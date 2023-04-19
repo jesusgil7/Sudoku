6ad169c23dd43301bdf9e99fce832aa63725786d
jesusgil7 <jesus.david.gil@hotmail.com>
2023-04-18 23:02:46 -0400

    Methods Added

--- board.py
+++ board.py
@@ -1,3 +1,36 @@
 
 class Board:
-    pass
+    def __init__(self, width, height, screen, difficulty):
+        pass
+    def draw(self):
+        pass
+    def select(self, row, col):
+        pass
+    def click(self, x, y):
+        pass
+
+    def clear(self):
+        pass
+
+    def sketch(self):
+        pass
+
+    def place_number(self):
+        pass
+
+    def place_number(self):
+        pass
+    def reset_to_original(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def updated_board(self):
+        pass
+    def find_empty(self):
+        pass
+
+    def check_board(self):
+        pass
+\
--- cell.py
+++ cell.py
@@ -1,2 +1,12 @@
 class Cell:
-    pass
+    def __init__(self, value, row, col, screen):
+        pass
+
+    def set_cell_value(self, value):
+        pass
+
+    def set_sketch_value(self,value):
+        pass
+
+    def draw(self):
+        pass
