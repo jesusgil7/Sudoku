95b2694bdc98a84aebe7dda53c4a8035f8d37e36
aidan <aidanboudreau@ufl.edu>
2023-04-25 12:38:28 -0400

    revised code

--- cell.py
+++ cell.py
@@ -1,12 +1,15 @@
 class Cell:
     def __init__(self, value, row, col, screen):
-        pass
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
 
     def set_cell_value(self, value):
-        pass
+        self.value = value
 
     def set_sketch_value(self,value):
-        pass
+        self.sketch_value = value
 
     def draw(self):
-        pass
+        self.screen.blit(self.value, ((self.column + 1) * 50 + 15, (self.row + 1) * 50))
